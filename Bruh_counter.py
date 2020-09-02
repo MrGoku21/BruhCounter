9a2f6963dfc4c07facb707221eb009d023b3c0ed
Isaac <47527704+MrGoku21@users.noreply.github.com>
2020-09-02 13:09:32 +0200

    Update Bruh_counter.py

--- Bruh_counter.py
+++ Bruh_counter.py
@@ -9,7 +9,7 @@ bot.message_counter = 0
 
 @bot.event
 async def on_ready():
-    print ("Systemet kjører nå!\nKlar til bruk kaptein!")
+    print ("Bot online!")
     await bot.change_presence(activity=discord.Streaming(name="Bruh this is slavery", url="https://www.twitch.tv/Twitch"))
 
 @bot.event
@@ -17,7 +17,7 @@ async def on_message(message):
     user = message.author
     if message.content.startswith("bruh"):
         embed=discord.Embed(title="Bruh Counter intensifies", description=f"{user} said the word Bruh! \n Bless this dude!", color=0xb1f1c0)
-        embed.set_footer(text="Made by Marceline <3")
+        embed.set_footer(text="Made by Caasi <3")
         await message.channel.send(embed=embed)
         bot.message_counter += 1
     await bot.process_commands(message)
@@ -27,7 +27,7 @@ async def bruh(ctx):
     embed=discord.Embed(title="Bruh Counter", description="The word bruh has been said {} times!".format(ctx.bot.message_counter) , color=0xefe0b4)
     embed.set_thumbnail(url="https://steamuserimages-a.akamaihd.net/ugc/958603887337454582/7ED14D409F60454623CFC1A6A1DFAEFFDF591460/")
     embed.add_field(name="Conclusion", value="The word bruh is overrated.", inline=True)
-    embed.set_footer(text="Made by Marceline <3 (Caasi)")
+    embed.set_footer(text="Made by Caasi")
     await ctx.send(embed=embed)   
 
 bot.run(" YOUR CLIENT_ID GOES HERE! ")                                                                                                                   
